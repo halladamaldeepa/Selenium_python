4cba9097f5f9071346afcfb3b912b36609fc5a78
halladamaldeepa <144892144+halladamaldeepa@users.noreply.github.com>
2023-10-13 16:46:50 +0530

    Add files via upload

---
+++ tasl_sele_2.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.implicitly_wait(10)
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+#sleep(4)
+driver.find_element(By.XPATH,'/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input').send_keys("Admin")
+#sleep(4)
+driver.find_element(By.NAME,"password").send_keys("admin123")
+#sleep(4)
+driver.find_element(By.XPATH,'/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+#sleep(4)
+actual_title=driver.title
+exp_title="OrangeHRM"
+
+if actual_title==exp_title:
+    print("login test passed")
+else:
+    print("login test failed")
+print("current url of the webpage is:",driver.current_url)
+driver.close()
